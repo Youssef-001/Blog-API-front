78e82c03f642277fc9c472011277666ec538165f
yusef <youssefsamir432@gmail.com>
2024-12-19 06:54:26 +0200

    Setup pagination in react

--- src/components/blog/Blog.jsx
+++ src/components/blog/Blog.jsx
@@ -4,18 +4,23 @@ import {useState, useEffect} from 'react';
 import Post from '../post/Post'
 import classes from './blog.module.css'
 import Header from '../header/Header'
+import { useLocation } from 'react-router-dom';
 
 function Blog()
 {
+  
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
-    const [data, setData] = useState(null);
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState(null);
+  const location = useLocation();
+  const queryParams = new URLSearchParams(location.search);
+  const page = queryParams.get('page') || 1;
 
    useEffect(() => {
     async function fetchPosts() {
         try {
-            const response = await fetch('http://localhost:4000/posts');
+            const response = await fetch(`http://localhost:4000/posts?page=${page}`);
             if (!response.ok)
             {
                 throw new Error(`HTTP error: Status ${response.status}`);
