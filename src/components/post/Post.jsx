6f433088fa1ab10b889a89218f9f219f83f4959a
yusef <youssefsamir432@gmail.com>
2024-12-20 08:34:31 +0200

    Add loading spinner

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import {useState} from 'react'
 import classes from './post.module.css'
-import ContentComponent from '../ContentComponent'
+import ContentComponent from '../ContentComponent';
+import ReactLoading from 'react-loading';
+
+const Example = ({ type, color }) => (
+    <ReactLoading type={type} color={color} height={200} width={200} />
+);
 
 function Post({date, thumbnail, title, description, postId})
 {
+    const [isLoading, setIsLoading] = useState(true);
+
+    const handleImageLoad = () => {
+        setIsLoading(false); 
+    };
     
     return (<div className={`${classes.post}`}>
 
@@ -14,11 +24,12 @@ function Post({date, thumbnail, title, description, postId})
             <a className={`${classes.link}`} href = {"/blog/"+postId}>{date}</a>
         </time>
 
-        <img className={`${classes.thumbnail}`} src={thumbnail} alt="" />
+        <img className={`${classes.thumbnail}`} src={thumbnail} onLoad={handleImageLoad} alt="" />
         
         </a>
 
         <h2>{title}</h2>
+        {isLoading && <div className={`${classes.circular_spinner}`}></div>} 
 
 
     </div>)
